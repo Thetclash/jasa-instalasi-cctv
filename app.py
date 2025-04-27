84328ad8932f8fde4088a9ec1a6a505079c10840
Thetclash <youremail@gmail.com>
2025-04-27 23:03:18 +0700

    update port setting for Railway

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    from os import getenv
-    port = int(getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
