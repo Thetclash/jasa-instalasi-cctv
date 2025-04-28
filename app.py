624a76864f45d5f4924ac2a70e61962fa8965ba9
Thetclash <youremail@gmail.com>
2025-04-28 07:26:24 +0700

    Fix: Proper Procfile and app.py structure

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from flask import Flask, render_template
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("index.html")  # ini buat buka tampilan jasa kamu
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
